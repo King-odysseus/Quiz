085c3a2fb0790b464776520ebd977f083895dbef
MegaMind <wingzatari@gmail.com>
2025-12-06 01:42:45 +0000

    first commit

---
+++ quiz.js
@@ -0,0 +1,8 @@
+//select the Score Display
+
+const scoreElement = document.getElementById("score");
+const progressBar = document.getElementById("progress-bar");
+const currentQuestion = document.getElementById("current-question");
+const percentage = document.getElementById("precentage");
+const questionText = document.getElementById('question-text');
+const 
